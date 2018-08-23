bc24ee981ef5110086760640a5915ccfce3a1e93
Thiago Y. Tachibana <30479132+thiagotachibana@users.noreply.github.com>
2018-08-23 18:50:28 -0300

    Add files via upload

---
+++ monitoria3/GrowthIID.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 23 16:29:09 2018
+
+@author: thiagoyuditachibana
+"""
+import numpy as np
+
+class GrowthIID:
+    
+    
+    #Class constructor: variables are:
+    #eta - CRRA parameter
+    #beta - discount factor
+    #alpha - concavity parameter of production function
+    #delta - depreciation rate
+    #probz - vector with shock probabilities
+    #valz - vector with shock realisation values
+    #kgrid - grid with possible choices of capital
+    
+    def __init__(self, eta, beta, alpha, delta, probz, valz, kgrid):
+        self.eta, self.beta, self.alpha, self.delta, self.probz, self.valz, self.kgrid =\
+        eta, beta, alpha, delta, probz, valz, kgrid
+        
+        #Computes expected value of shock
+        z_bar = np.dot(self.probz, self.valz)
+        
+        #Constructs dim Kprime x dim K x dim Z  K-array by replicating kgrid across columns and matrices
+        #also constructs dim Kprime x dim K x dim Z Z-matrix by replicating zvalz across rows and matrices
+        self.gridK, self.gridKprime, self.gridZ   = np.meshgrid(self.kgrid, self.kgrid, self.valz)
+        
+        #Steady state of nonstochastic model with shock at average
+        self.kss = ((z_bar*self.alpha)/((1/self.beta) + self.delta - 1))**(1/(1-self.alpha))
+    
+    #Function to reconstruct grids given a new vector of krigd
+    def reconstr_grid(self, kgrid):
+        self.kgrid = kgrid
+        #Constructs dim Kprime x dim K x dim Z  K-array by replicating kgrid across columns and matrices
+        #also constructs dim Kprime x dim K x dim Z Z-matrix by replicating zvalz across rows and matrices
+        self.gridK, self.gridKprime, self.gridZ   = np.meshgrid(self.kgrid, self.kgrid, self.valz)
+    
+    def utility(self, c):
+        return c**(1- self.eta)/(1-self.eta)
+    
+    def prodf(self, k):
+        return k**self.alpha
+    
+    
+    #Bellman operator: given a dim K x dim Z matrix V(K,Z), computes TV
+    def bellman(self, V):
+        #Computes EV(K',Z')=
+        EV = V @ self.probz
+        
+        #Reshapes EV to a cube
+        EV_reshaped = np.repeat(EV, len(self.kgrid)*len(self.valz)).reshape((len(self.kgrid), len(self.kgrid), len(self.valz) ))
+        
+        #Computes consumption
+        c = (1-self.delta)*self.gridK + self.gridZ*self.prodf(self.gridK) \
+        - self.gridKprime
+        
+        #Sets consumption close to zero if negative
+        c[c<=0] = 1e-10  
+        
+        #Computes utility
+        uu = self.utility(c)  + self.beta*EV_reshaped
+        
+        return [np.max(uu, axis = 0),uu]
+    
+    #Runs Bellman and also returns policy function 
+    #Value function iteration: Initial guess is a dimK x dimZ matrix V(K,z)
+    def value_iteration(self, V0, tol = 1e-3):
+        err = tol + 1
+        while err > tol:
+            Vnew, uu = self.bellman(V0)
+            err = np.max(np.abs(Vnew - V0))
+            V0 = Vnew
+            print(err)
+        
+        policy_index = np.argmax(uu, axis = 0)
+        policy_kprime = self.kgrid[policy_index]
+        
+        return [V0, policy_kprime]
+        
+    
+        
+    
+    
+        
+        
+
---
+++ monitoria3/run.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 23 18:13:03 2018
+
+@author: thiagoyuditachibana
+"""
+
+from GrowthIID import GrowthIID
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+from matplotlib import cm
+
+zgrid = np.array([1,2,3])
+economia1 =  GrowthIID(2, 0.96, 0.3, 0.25, np.array([1/2,1/4,1/4]), \
+                       zgrid, 17)
+
+kgrid =  np.linspace(0.5*economia1.kss, 2*economia1.kss, 300)
+
+economia1.reconstr_grid(kgrid)
+
+#Grids with k replicated across columns and z replicated across lines
+
+gridZ, gridK = np.meshgrid(zgrid, kgrid)
+
+#Initial guess for V0: consume all capital every period forever
+V0 = (1/(1-economia1.beta))*economia1.utility(gridK)
+
+V0, kprime = economia1.value_iteration(V0, 1e-5)
+
+#Capital policy function
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.plot_surface(gridK, gridZ, kprime, cmap = cm.coolwarm)
+ax.set_xlabel('$k_t$')
+ax.set_ylabel('$z_t$')
+ax.set_zlabel('$k^\\prime(k,h)$')
+plt.savefig('capitalpolicy.pdf')
+plt.show()
